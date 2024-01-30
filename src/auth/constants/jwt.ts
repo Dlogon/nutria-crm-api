ba53139b7bd4e25663770af43ac6cfbd5aaffcda
Diego <dlogon@outlook.com>
2024-01-30 14:07:17 -0600

    add jwt constants

--- src/auth/constants/jwt.ts
+++ src/auth/constants/jwt.ts
@@ -1,1 +1,4 @@
-export const JWT_TTL = '60d';
+export const JWT_TTL = '10s';
+export const JWT__REFRESH_TTL = '7d';
+export const JWT_SECRET_ENV = 'JWT_SECRET';
+export const JWT__REFRESH_SECRET_ENV = 'JWT_REFRESH_SECRET';
