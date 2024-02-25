61a2989b2e9e620527de98333f6f33a2bbd673ef
Diego <dlogon@outlook.com>
2024-02-25 17:27:54 -0600

    add leads module to typeorm

--- src/leads/entities/lead.entity.ts
+++ src/leads/entities/lead.entity.ts
@@ -1,1 +1,8 @@
-export class Lead {}
+import { BaseModelEntity } from 'src/common/database/entities/base.entity';
+import { softDeteles } from 'src/common/database/entities/softDeletes';
+import { Column, Entity } from 'typeorm';
+@Entity()
+export class Lead extends BaseModelEntity implements softDeteles {
+  @Column({ nullable: true })
+  deletedAt: Date;
+}
