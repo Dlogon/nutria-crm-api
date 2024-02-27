070117252255093781d7012862ded41e286364eb
Diego <dlogon@outlook.com>
2024-02-26 23:09:56 -0600

    define lead entity

--- src/leads/entities/lead.entity.ts
+++ src/leads/entities/lead.entity.ts
@@ -1,8 +1,26 @@
 import { BaseModelEntity } from 'src/common/database/entities/base.entity';
+import { createdBy } from 'src/common/database/entities/createdBy';
 import { softDeteles } from 'src/common/database/entities/softDeletes';
 import { Column, Entity } from 'typeorm';
 @Entity()
-export class Lead extends BaseModelEntity implements softDeteles {
+export class Lead extends BaseModelEntity implements softDeteles, createdBy {
+  @Column({ nullable: true })
+  userId: number;
   @Column({ nullable: true })
   deletedAt: Date;
+
+  @Column({ nullable: true })
+  firsName: string;
+  @Column({ nullable: false })
+  lastName: string;
+
+  @Column({ nullable: true })
+  jobTitle: string;
+
+  @Column({ nullable: true })
+  officePhone: string;
+  @Column({ nullable: true })
+  mobilePhone: string;
+
+  source: string;
 }
