f23032c7820b1fdf2704244ff0c694bfa90e0785
Diego <dlogon@outlook.com>
2024-02-28 14:26:15 -0600

    lead service with typeorm

--- src/leads/leads.service.ts
+++ src/leads/leads.service.ts
@@ -1,48 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLeadDto } from './dto/create-lead.dto';
 import { UpdateLeadDto } from './dto/update-lead.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lead } from './entities/lead.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LeadsService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Lead)
+    private leadRepository: Repository<Lead>,
+  ) {}
 
-  async create(createLeadDto: CreateLeadDto) {
-    return createLeadDto;
-    // const lead = await this.prisma.lead.create({
-    //   data: {
-    //     ...createLeadDto,
-    //   },
-    // });
-    // return lead;
+  async create(createLeadDto: CreateLeadDto): Promise<{ id: number }> {
+    const lead = await this.leadRepository.create({ ...createLeadDto });
+    await this.leadRepository.save(lead);
+    return { id: lead.id };
   }
 
   findAll() {
-    return {}; // this.prisma.lead.findMany();
+    return this.leadRepository.find({});
   }
 
-  findOne(id: number) {
-    return {};
-    // this.prisma.lead.findUnique({
-    //   where: {
-    //     id: id,
-    //   },
-    // });
+  async findOne(id: number) {
+    const lead = await this.leadRepository.findOneBy({ id });
+    if (!lead) throw new NotFoundException();
+    return lead;
   }
 
   async update(id: number, updateLeadDto: UpdateLeadDto) {
-    return {};
-    // const lead = this.prisma.lead.update({
-    //   where: {
-    //     id: id,
-    //   },
-    //   data: {
-    //     ...updateLeadDto,
-    //   },
-    // });
-    // return lead;
+    let lead = await this.findOne(id);
+    if (!lead) throw new NotFoundException('Record not found');
+
+    lead = await this.leadRepository.save({
+      id: id,
+      ...updateLeadDto,
+    });
+    return lead;
   }
 
-  remove(id: number) {
-    return id;
+  async remove(id: number) {
+    const leadDeleteResult = await this.leadRepository.softDelete(id);
+    if (!leadDeleteResult.affected) {
+      throw new NotFoundException('Can not delete this record');
+    }
+    return {};
   }
 }
