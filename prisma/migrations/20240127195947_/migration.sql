64c1bdb6b72ba5984869ce26da724743e9323b91
Diego <dlogon@outlook.com>
2024-03-08 10:59:23 -0600

    delete prisma folder

--- prisma/migrations/20240127195947_/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Lead` ADD COLUMN `deletedAt` DATETIME(3) NULL,
-    ADD COLUMN `userId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Lead` ADD CONSTRAINT `Lead_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
