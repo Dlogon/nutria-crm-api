03a9c2ec2e93ea01d1fcc08e632669928609df3d
Diego <dlogon@outlook.com>
2024-03-09 21:57:08 -0600

    convert lead to acc dto

---
+++ src/leads/dto/convert-lead-to-acc.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsEnum, IsOptional, IsString, IsUrl } from 'class-validator';
+import { AccountType, Industry } from '@app/accounts/enums';
+
+export class convertLeadToAccountDto {
+  @IsString()
+  @IsOptional()
+  alias?: string;
+
+  @IsString()
+  @IsOptional()
+  jobTitle?: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsUrl()
+  @IsOptional()
+  website?: string;
+
+  @IsEnum(Industry)
+  @IsOptional()
+  industry?: string;
+  @IsOptional()
+  @IsEnum(AccountType)
+  type?: string;
+}
