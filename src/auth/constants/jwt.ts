e22a779b5fa7ecbb620c13017f6ee203e579caea
Diego <dlogon@outlook.com>
2024-03-22 19:37:47 -0600

    access token ttl on seconds and return on login

--- src/auth/constants/jwt.ts
+++ src/auth/constants/jwt.ts
@@ -1,4 +1,4 @@
-export const JWT_TTL = '3600s';
-export const JWT__REFRESH_TTL = '7d';
+export const JWT_TTL = 60 * 60 * 24;
+export const JWT__REFRESH_TTL = 60 * 60 * 24 * 7;
 export const JWT_SECRET_ENV = 'JWT_SECRET';
 export const JWT__REFRESH_SECRET_ENV = 'JWT_REFRESH_SECRET';
